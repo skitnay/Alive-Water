3b5547e5f30f350a360a66ea83275b43a7d6c1b8
skitnay <sima9729@gmail.com>
2023-04-21 21:56:17 +0700

    Check git

--- alivewater/pages/_document.js
+++ alivewater/pages/_document.js
@@ -26,8 +26,8 @@ export default function Document() {
 				<Main />
 				<NextScript />
 				<script
-				dangerouslySetInnerHTML={{
-				__html: `
+					dangerouslySetInnerHTML={{
+						__html: `
 					(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
 					m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
 					(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
@@ -35,17 +35,17 @@ export default function Document() {
 					ym(83357116, "init", {
 							clickmap:true,
 							trackLinks:true,
-							accurateTrackBounce:true
+							accurateTrackBounce:true,
 							webvisor:true
 					});
 				`,
-				}}
-			/>
-			<noscript>
-			<div>
-            <img src="https://mc.yandex.ru/watch/83357116" style={{ position:'absolute', left:'-9999px' }} alt="" />
-			</div>
-			</noscript>
+					}}
+				/>
+				<noscript>
+					<div>
+						<img src="https://mc.yandex.ru/watch/83357116" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+					</div>
+				</noscript>
 			</body>
 		</Html>
 	)
