fbc451fddb3f92cddf60917b8c574d130cdadd3c
skitnay <sima9729@gmail.com>
2023-04-22 16:07:24 +0700

    Fixing

--- alivewater/pages/_document.js
+++ alivewater/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
 	return (
 		<Html lang='ru'>
 			<Head>
-
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
 				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
