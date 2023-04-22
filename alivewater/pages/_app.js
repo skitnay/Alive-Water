981f1d896c406868b2c80a69f7c65c2a5498fc94
skitnay <sima9729@gmail.com>
2023-04-22 13:40:20 +0700

    Fix Fonts

--- alivewater/pages/_app.js
+++ alivewater/pages/_app.js
@@ -4,11 +4,6 @@ import { AnimatePresence } from "framer-motion";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import { Analytics } from '@vercel/analytics/react';
-// import localFont from 'next/font/local'
-
-// const myFont = localFont({ src: './my-font.woff2' })
-
-
 
 export default function MyApp({ Component, pageProps, router }) {
 	const url = `${router.route}`;
