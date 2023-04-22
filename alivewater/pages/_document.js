70ae7c0b4f71ec482c9eb2107f1cb2791cb8304b
skitnay <sima9729@gmail.com>
2023-04-22 15:32:23 +0700

    Color and html lang fix

--- alivewater/pages/_document.js
+++ alivewater/pages/_document.js
@@ -2,11 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
 	return (
-		<Html>
+		<Html lang='ru'>
 			<Head>
+
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+				<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" />
 				<link rel="shortcut icon" href="/static/favicon.ico" />
 				<link rel="shortcut icon" href="/static/favicon.png" />
 				<meta name="theme-color" content="#263c5c" />
