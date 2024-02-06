e459f61fed4e510a989bba783150035e4396539a
skitnay <sima9729@gmail.com>
2024-02-06 15:50:49 +0300

    Open Graph

--- alivewater/pages/_document.js
+++ alivewater/pages/_document.js
@@ -17,15 +17,13 @@ export default function Document() {
 				<meta charSet="UTF-8" />
 				<meta name="format-detection" content="telephone=no" />
 				<meta name="google-site-verification" content="gMuzE89aJAghIlUGo0fbZ0PECVkSjj0FvKx3lDteqmM" />
-				<meta name="robots" />
-				<meta name="description" content="Питьевая вода высокого качества в Новосибирске!" />
-				<meta name='wmail-verification' content='102d9e06e62e85f7a7f2bd41e6ce0ea0' />
 				<meta property="og:title" content="Живая Вода Новосибирск" />
 				<meta property="og:description" content="Питьевая вода высокого качества в Новосибирске!" />
 				<meta property="og:image" content="/public/images/staff/site-prev.jpg" />
+				<meta name="robots" />
+				<meta name="description" content="Питьевая вода высокого качества в Новосибирске!" />
+				<meta name='wmail-verification' content='102d9e06e62e85f7a7f2bd41e6ce0ea0' />
 				<link rel="image_src" href="/public/images/staff/site-prev.jpg" />
-				<meta property="og:title" content="Живая вода Новосибирск" />
-				<meta property="og:image" content="/public/images/staff/site-prev.jpg" />
 			</Head>
 			<body>
 				<Main />
