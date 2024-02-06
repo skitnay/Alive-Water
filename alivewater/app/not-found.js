deba25adaf997305010320f1499a6d736718d26f
skitnay <sima9729@gmail.com>
2024-02-06 05:50:21 +0300

    404 page style

--- alivewater/app/not-found.js
+++ alivewater/app/not-found.js
@@ -1,13 +1,29 @@
 import Link from 'next/link'
 import classes from "../styles/layout.module.scss";
 import "../styles/style.scss";
+import Image from 'next/image';
+import icon from "/public/images/mobile/Apple-touch-ipad-retina.png"
 
+// const myLoader = ({ src, width, quality }) => {
+// 	return `/images/${src}?w=${width}&q=${quality || 75}`;
+// };
 
 export default function NotFound() {
 	return (
 		<div className={classes.notFound}>
-			<p className={classes.notFound__text}>Такой страницы нет на нашем сайте</p>
-			<Link href="/" className={classes.notFound__link}>Вернуться на начальную страницу</Link>
+			<Image
+				// loader
+				src={icon}
+				alt=""
+				width={167}
+				height={167}
+			// priority
+			/>
+			<div>
+				<p className={classes.notFound__title}>Упс...</p>
+				<p className={classes.notFound__text}>Такой страницы нет на нашем сайте</p>
+				<Link href="/" className={classes.notFound__link}>Вернуться на начальную страницу</Link>
+			</div>
 		</div>
 	)
 }
